2719200f053ddc7cb7fc38c7b53e98da76939a9d
adamParker96 <52212988+adamParker96@users.noreply.github.com>
2025-06-14 21:44:13 -0700

    Create userModel.js

---
+++ apps/backend/src/services/user-service/source/models/userModel.js
@@ -0,0 +1,30 @@
+const db = require('../db');
+
+async function getAllUsers() {
+  const { rows } = await db.query('SELECT * FROM users ORDER BY created_at DESC');
+  return rows;
+}
+
+async function getUserByID(id) {
+  const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [id]);
+  return rows[0];
+}
+
+async function getUserByName(name){
+  const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [name]);
+  return rows[0];
+}
+
+async function createUser({ name, id, email}) {
+  const { rows } = await db.query(
+    'INSERT INTO users (name, id, email) VALUES ($1, $2, $3) RETURNING *',
+    [name, id, email]
+  );
+  return rows[0];
+}
+
+async function deleteUser(id) {
+  await db.query('DELETE FROM users WHERE id = $1', [id]);
+}
+
+module.exports = { getAllUsers, getUserByID, getUserByName, createUser, deleteUser, updateUser };
