16ff15913ab6ce379eefb8a138ada417defe4855
adamParker96 <52212988+adamParker96@users.noreply.github.com>
2025-06-16 23:45:59 -0700

    Update cache.js

--- apps/backend/src/services/user-service/source/cache.js
+++ apps/backend/src/services/user-service/source/cache.js
@@ -1,7 +1,10 @@
 const redis = require('redis');
 
 const client = redis.createClient({
-  url: process.env.REDIS_URL,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  }
 });
 
 client.on('error', (err) => console.log('Redis Client Error', err));
