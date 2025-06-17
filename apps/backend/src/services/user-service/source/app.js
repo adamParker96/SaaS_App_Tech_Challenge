cfb62cc1a3175e01efd11fb9c273478244029dfd
adamParker96 <52212988+adamParker96@users.noreply.github.com>
2025-06-16 23:45:00 -0700

    Update app.js

--- apps/backend/src/services/user-service/source/app.js
+++ apps/backend/src/services/user-service/source/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const fileRoutes = require('./routes/userRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 app.use(express.json());
