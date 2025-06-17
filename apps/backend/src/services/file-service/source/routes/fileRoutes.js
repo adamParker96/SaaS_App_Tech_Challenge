f3f7efce3026e85d2f6b9c706353f917d9084787
adamParker96 <52212988+adamParker96@users.noreply.github.com>
2025-06-17 00:31:21 -0700

    Update fileRoutes.js

--- apps/backend/src/services/file-service/source/routes/fileRoutes.js
+++ apps/backend/src/services/file-service/source/routes/fileRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const controller = require('../controllers/fileController');
 
 // Read
-router.get('/', controller.getAll);
-router.get('/:id', controller.getById);
 router.get('/name/:filename', controller.getByName);
+router.get('/:id', controller.getById);
+router.get('/', controller.getAll);
 
 // Create
 router.post('/', controller.upload); //  TODO: add middleware like multer for file handling
