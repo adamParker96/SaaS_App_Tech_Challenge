3dba445ad2289756c84d8e691d1c11b72faa1553
adamParker96 <52212988+adamParker96@users.noreply.github.com>
2025-06-16 23:08:53 -0700

    Update userSchema.js

--- apps/backend/src/services/user-service/source/schemas/userSchema.js
+++ apps/backend/src/services/user-service/source/schemas/userSchema.js
@@ -1,11 +1,19 @@
-// schemas/userSchema.js
 const Joi = require('joi');
 
+// For creating a user (POST /users)
 const createUserSchema = Joi.object({
+  id: Joi.string().guid({ version: 'uuidv4' }).required(),
   name: Joi.string().min(1).max(100).required(),
-  email: Joi.string().email().required(),
+  email: Joi.string().email().required()
+});
+
+// For updating a user (PUT /users/:id)
+const updateUserSchema = Joi.object({
+  name: Joi.string().min(1).max(100).required(),
+  email: Joi.string().email().required()
 });
 
 module.exports = {
   createUserSchema,
+  updateUserSchema
 };
