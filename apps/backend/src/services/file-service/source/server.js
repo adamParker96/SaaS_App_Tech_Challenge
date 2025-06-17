1535f3ecaa0cdf451a62d6b7eb84f23fa2ae34dc
adamParker96 <52212988+adamParker96@users.noreply.github.com>
2025-06-17 00:33:10 -0700

    Update server.js

--- apps/backend/src/services/file-service/source/server.js
+++ apps/backend/src/services/file-service/source/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`File service running on port ${PORT}`);
