3672d4485bfa44a96285e47a38202be18866d909
adamParker96 <52212988+adamParker96@users.noreply.github.com>
2025-06-18 12:16:12 -0700

    Update articleRoutes.js

--- apps/backend/src/services/article-service/source/routes/articleRoutes.js
+++ apps/backend/src/services/article-service/source/routes/articleRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const articleController = require('../controllers/articleController');
 const validateSanitize = require('../validation-sanitation/validationSanitation');
 const { createArticleSchema, updateArticleSchema } = require('../validation-sanitation/schema');
+const checkApiKey = require('../validation-sanitation/checkApiKey');
+
 
 router.get('/', articleController.getAllArticles);
 router.get('/:id', articleController.getArticleById);
@@ -10,16 +12,18 @@ router.get('/title/:title', articleController.getArticleByName);
 
 router.post(
   '/',
+  checkApiKey,
   validateSanitize(createArticleSchema, { sanitize: ['title', 'content'] }),
   articleController.createArticle
 );
 
 router.put(
   '/:id',
+  checkApiKey,
   validateSanitize(updateArticleSchema, { sanitize: ['title', 'content'] }),
   articleController.updateArticle
 );
 
-router.delete('/:id', articleController.removeArticle);
+router.delete('/:id', checkApiKey, articleController.removeArticle);
 
 module.exports = router;
