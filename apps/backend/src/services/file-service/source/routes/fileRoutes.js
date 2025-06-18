a782ac228ce7c18df242d72807cdcdd5b5f8b910
adamParker96 <52212988+adamParker96@users.noreply.github.com>
2025-06-18 12:17:41 -0700

    Update fileRoutes.js

--- apps/backend/src/services/file-service/source/routes/fileRoutes.js
+++ apps/backend/src/services/file-service/source/routes/fileRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/fileController');
 const { validateCreateFile, validateUpdateFile } = require('../validation/fileValidation');
+const checkApiKey = require('../validation-sanitation/checkApiKey');
+
 
 // Read
 router.get('/name/:filename', controller.getByName);
@@ -9,12 +11,12 @@ router.get('/:id', controller.getById);
 router.get('/', controller.getAll);
 
 // Create
-router.post('/', validateCreateFile, controller.upload);  //  Add validation middleware
+router.post('/', checkApiKey, validateCreateFile, controller.upload);  //  TODO: Add validation middleware
 
 // Update
-router.put('/:id', validateUpdateFile, controller.update);
+router.put('/:id', checkApiKey,validateUpdateFile, controller.update);
 
 // Delete
-router.delete('/:id', controller.remove);
+router.delete('/:id', checkApiKey, controller.remove);
 
 module.exports = router;
